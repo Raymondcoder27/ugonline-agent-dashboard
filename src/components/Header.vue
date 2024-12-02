87d5299470a41571ae876ff680240f7b00e00f5e
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-02 13:12:36 +0300

    reworks agent dashboard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,24 +12,11 @@ const sideMenuOpen = ref(useStorage("sms", true));
 
 <template>
   <nav
-    class="top-0 relative z-50 w-full md:flex-row md:flex-nowrap flex items-center px-2 py-3 navbar-expand-lg border-b border-primary-200 bg-white"
+    class="top-0 relative z-50 w-full md:flex-row md:flex-nowrap md:justify-start flex items-center justify-between px-2 py-3 navbar-expand-lg border-b border-primary-200 bg-white"
   >
     <div class="flex items-center justify-between w-full px-4">
       <!-- Left side: Logo and sidebar toggle button -->
       <div class="flex items-center">
-        <span
-          @click="sideMenuOpen = !sideMenuOpen"
-          class="text-primary text-xl block cursor-pointer mr-4"
-        >
-          <i
-            :class="
-              sideMenuOpen
-                ? 'transition ease-in-out rotate-0 duration-200'
-                : 'transition ease-in-out -rotate-180 duration-200'
-            "
-            class="fa-solid fa-angles-left"
-          ></i>
-        </span>
         <div class="flex items-center">
           <span
             class="my-auto mx-2 text-white rounded font-bold text-xl shadow"
@@ -41,21 +28,9 @@ const sideMenuOpen = ref(useStorage("sms", true));
         </div>
       </div>
 
-      <!-- Centered TopMenu -->
-      <div class="flex-1 flex justify-center items-center">
+      <!-- Right side: Center TopMenu -->
+      <div class="flex items-center justify-end ml-auto">
         <TopMenu />
-      </div>
-
-      <!-- Right side: Person icon and logout button -->
-      <div class="flex items-center">
-        <i class="fa-solid fa-user text-xl mr-4"></i>
-        <p class="text-sm font-bold">Username</p>
-        <button
-          class="px-2 border border-primary-500 text-primary-700 rounded text-xs hover:bg-primary hover:text-white"
-          @click="logout"
-        >
-          Logout
-        </button>
       </div>
     </div>
   </nav>
