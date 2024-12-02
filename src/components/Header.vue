212d4c880d074d6a2193f1f9bf50448197bdb2dc
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-02 12:44:55 +0300

    reworks agent header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Logo from "@/assets/images/banner.png"
-import TopMenu from "@/components/TopMenu.vue"
+import Logo from "@/assets/images/banner.png";
+import TopMenu from "@/components/TopMenu.vue";
 import { useStorage } from "@vueuse/core";
 import { computed, unref, ref } from "vue";
 import type { ComputedRef } from "vue";
@@ -11,31 +11,43 @@ const sideMenuOpen = ref(useStorage("sms", true));
 </script>
 
 <template>
-  <nav class="top-0 relative z-50 w-full md:flex-row md:flex-nowrap md:justify-start flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg border-b border-primary-200 bg-white">
-    <div class="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap px-4">
+  <nav
+    class="top-0 relative z-50 w-full md:flex-row md:flex-nowrap md:justify-start flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg border-b border-primary-200 bg-white"
+  >
+    <div
+      class="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap px-4"
+    >
       <!-- Logo -->
       <div class="flex">
         <div class="w-full">
           <div class="flex flex-grow">
-             <span @click="sideMenuOpen = !sideMenuOpen" class="text-primary text-xl block cursor-pointer">
-                <i :class="sideMenuOpen ? 'transition ease-in-out rotate-0 duration-200' : 'transition ease-in-out -rotate-180 duration-200'"
-                   class="fa-solid fa-angles-left"></i>
-             </span>
+            <span
+              @click="sideMenuOpen = !sideMenuOpen"
+              class="text-primary text-xl block cursor-pointer"
+            >
+              <i
+                :class="
+                  sideMenuOpen
+                    ? 'transition ease-in-out rotate-0 duration-200'
+                    : 'transition ease-in-out -rotate-180 duration-200'
+                "
+                class="fa-solid fa-angles-left"
+              ></i>
+            </span>
             <div class="flex">
               <div class="w-full">
-               <span class="my-auto mx-2 text-white rounded font-bold text-xl shadow">
-                <b class="rounded-l bg-black-800 p-1">ug</b>
-                <b class="bg-yellow-500 p-1">On</b>
-                <b class="rounded-r bg-primary-700 p-1">line</b>
-              </span>
+                <span
+                  class="my-auto mx-2 text-white rounded font-bold text-xl shadow"
+                >
+                  <b class="rounded-l bg-black-800 p-1">ug</b>
+                  <b class="bg-yellow-500 p-1">On</b>
+                  <b class="rounded-r bg-primary-700 p-1">line</b>
+                </span>
               </div>
-
-              
             </div>
+            <!-- <p>Hello world</p> -->
             <TopMenu />
-              <!-- <p>Hello world</p> -->
           </div>
-
         </div>
       </div>
     </div>
