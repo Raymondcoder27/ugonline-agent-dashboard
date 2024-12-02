61b5654f2ef19560c180443e0795ad5c927cfd34
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-02 18:16:32 +0300

    reworks billing

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -45,7 +45,7 @@ const sideMenu: SideMenuLink[] = [
   //add activity log
   {
     name: "app-activity-log",
-    label: "SUBMISSION",
+    label: "SUBMISSIONS",
     iconClass: "fa-solid fa-clipboard-list",
   },
 ];
