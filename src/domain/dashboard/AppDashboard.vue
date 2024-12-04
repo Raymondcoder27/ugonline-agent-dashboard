22815496af37bd69053bce6c2b71daf69f496a8c
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-04 18:43:49 +0300

    reworks billing

--- src/domain/dashboard/AppDashboard.vue
+++ src/domain/dashboard/AppDashboard.vue
@@ -152,11 +152,12 @@ watch(
     <!-- <div class="block"> -->
   <!-- Styled Search Bar -->
   <div class="flex px-4 py-3 bg-white shadow-md rounded-lg justify-between items-center mb-6">
-   <div>
+   <div class="w-2/3">
     <input
       type="text"
       placeholder="Search..."
       class="w-full md:w-2/3 lg:w-1/2 px-4 py-2 border border-gray-300 rounded-lg text-sm focus:ring-2 focus:ring-blue-500"
+
     />
     <button
       class="ml-4 px-6 py-2 bg-red-700 text-white rounded-md text-sm hover:bg-primary-600 transition duration-300 ease-in-out"
@@ -165,7 +166,7 @@ watch(
       Search
     </button>
    </div>
-   <div>BALANCE: 16,000,000/=</div>
+   <div class="font-semibold">BALANCE: 16,000,000/=</div>
   </div>
 
   <!-- Service Cards Section -->
